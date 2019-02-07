a8aceac881098e083017442530ce21752b626858
Haydar77 <haydarshariff@gmail.com>
2019-02-06 23:37:03 -0500

    Simple Login App

---
+++ app/routes/index.server.routes.js
@@ -0,0 +1,16 @@
+﻿module.exports = function (app) {
+
+    //load the controllers
+    var index = require('../controllers/index.server.controller');
+    var thankyou = require('../controllers/thankyou.server.controller');
+    var feedback = require('../controllers/feedback.server.controller');
+
+    //handle the routing of get and post request
+    app.get('/', index.render);
+    app.post('/', index.render);
+    app.get('/index', index.render);
+    app.post('/index', index.render);
+    app.get('/thankyou', thankyou.render);
+    app.get('/feedback', feedback.render);
+    app.post('/feedback', thankyou.render);
+};
